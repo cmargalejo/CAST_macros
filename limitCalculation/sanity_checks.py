2a5040038ad1bd8753904c876b2ae6c9ea9912a0
@cmargalejo <cristinamargalejoblasco@gmail.com>
2023-11-30 20:39:03 +0100

    Some minor updates in sanity checks

--- limitCalculation/sanity_checks.py
+++ limitCalculation/sanity_checks.py
@@ -1,7 +1,9 @@
 from unbinned_likelihood_CAST_g4_datasets import *
 #In the inner 20 mm side square I have 40*40=1600 "pixels"
-c = candidate_weights(dataset_Ar1, 0, 0)*4.0 # 4 cm^2 is the area of my 2x2 cm rectangle
-print("Weight of a candidate at (0,0): ", c)
+c = candidate_weights(dataset_Ar1, 0, 0)*4.0 # 4 cm^2 is the area of my 2x2 cm rectangle going form 10mm to 10 mm in X and Y
+print("Weight of a candidate at (0,0) if applied to the full area: ", c)
+c1 = candidate_weights(dataset_Ar1, 0, 0)*0.02*0.02 # 4 cm^2 is the area of my 2x2 cm rectangle going form 10mm to 10 mm in X and Y
+print("Weight of a candidate at (0,0) if applied to a small pixel of 0.02x0.02 cm: ", c1)
 # now I do it for several squares to make it more realistic. Before I was using the interpolated value for the center and applying it to the full area --> >1
 # i need to define the coordinates of the center of each square
 n = 1000 #number of rectangles in each dimension
@@ -9,9 +11,12 @@ area = 4.0/n**2
 xMin = -10
 xMax = 10
 xSize = 2.0 / n
+c2 = candidate_weights(dataset_Xe, 0, 0)*area # 4 cm^2 is the area of my 2x2 cm rectangle going form 10mm to 10 mm in X and Y
+print("Weight of a candidate at (0,0) if applied to a pixel out of 1000x1000: ", c2)
 coord = np.linspace(xMin + xSize / 2.0, xMax - xSize / 2.0, n)
 sum = 0.0
 for x in coord:
     for y in coord:
         sum += candidate_weights(dataset_Xe, x, y)*area
 print(sum)
+#check also with convolution
